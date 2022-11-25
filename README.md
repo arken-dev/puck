d48bd85cef217bfa353e2b116083ad95cadf289e
Alexandre F Riveira <alexandre@objectdata.com.br>
2022-11-25 10:58:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# PUCK
+
+Minimal loader for arken.net.HttpServer whitout lua vm instane
+
+## BUILD
+
+- clone in package directory
+```
+cd arken
+git clone github.com:arken-dev/puck.git packages/puck
+```
+
+## LICENSE
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ main.cpp
@@ -0,0 +1,26 @@
+// Copyright 2016 The Arken Platform Authors.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include <arken/base>
+#include <arken/mvm.h>
+#include <arken/net/httpserver.h>
+
+using arken::mvm;
+using arken::net::HttpServer;
+
+int main(int argc, char * argv[])
+{
+
+   mvm::init(argc, argv);
+
+   auto server = new HttpServer("127.0.0.1", 2345);
+   server->setThreads(25);
+   server->setPid("tmp/pid/server.pid");
+   server->setDispatcher("lib.dispatcher");
+   server->addService("app/services");
+   server->start();
+
+   return 0;
+}
